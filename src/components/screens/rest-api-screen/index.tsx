b28f62d453d2ba4300e1a5452db1ea187b03d00c
7ime <lopatsin1990@gmail.com>
2020-10-15 13:16:43 +0300

    styles posts

--- src/components/screens/rest-api-screen/index.tsx
+++ src/components/screens/rest-api-screen/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import {upperFirst} from 'lodash'
 import {
-    Text, View
+    Text, View, ScrollView
 } from 'react-native'
 import {JsonPlaceholderAction, JsonPlaceholderSelector} from '../../../store/jsonplaceholder'
 import {useDispatch, useSelector} from 'react-redux'
@@ -23,7 +24,18 @@ const RestApiScreen = () => {
             {!posts && <Text>Loading...</Text>}
             {
                 posts && (
-                    <Text style={styles.title}>Rest Api Screen</Text>
+                    <ScrollView style={styles.list}>
+                        {
+                            posts.map(({id, title, body}) => {
+                                return (
+                                    <View key={id} style={styles.item}>
+                                        <Text numberOfLines={1} style={styles.title}>{title}</Text>
+                                        <Text numberOfLines={3} style={styles.text}>{upperFirst(body)}</Text>
+                                    </View>
+                                )
+                            })
+                        }
+                    </ScrollView>
                 )
             }
         </View>
--- src/components/screens/rest-api-screen/styles.ts
+++ src/components/screens/rest-api-screen/styles.ts
@@ -1,10 +1,25 @@
 import {StyleSheet} from 'react-native'
-import {FONTS} from '../../../styles/variables'
+import {COLORS, FONTS} from '../../../styles/variables'
 
 export default StyleSheet.create({
+    list: {
+        position: 'relative'
+    },
+    item: {
+        marginBottom: 10
+    },
     title: {
         fontFamily: FONTS.os_bold,
-        fontSize: 20
+        fontSize: 20,
+        marginBottom: 5,
+        textTransform: 'capitalize',
+    },
+    text: {
+        fontFamily: FONTS.os_regular,
+        fontSize: 10,
+        backgroundColor: COLORS.green_light_50,
+        padding: 8,
+        borderRadius: 5
     }
 })
 
