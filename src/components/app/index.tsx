3ab95e79bec13be0962bc5b708a41d54ea53ed83
7ime <lopatsin1990@gmail.com>
2020-10-18 09:31:53 +0300

    custom header

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-    StatusBar,
+    StatusBar
 } from 'react-native'
 import {createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from '../screens/home-screen'
@@ -8,6 +8,7 @@ import RestApiScreen from '../screens/rest-api-screen'
 import {ERootScreens} from '../../navigation/screens'
 import {COLORS} from '../../styles/variables'
 import {INavigation} from '../../navigation/model'
+import {StackHeaderProps} from '@react-navigation/stack/lib/typescript/src/types'
 
 const Stack = createStackNavigator<INavigation.RootStackParamList>()
 
@@ -19,7 +20,10 @@ const App = () => {
             <StatusBar barStyle="dark-content"
                        hidden={false}/>
             <Stack.Navigator initialRouteName={ERootScreens.Home} screenOptions={{
-                headerShown: false
+                header: (props: StackHeaderProps) => {
+                    return null
+                },
+                headerShown: true
             }}>
                 <Stack.Screen name={ERootScreens.Home} component={HomeScreen} options={{title: 'Home'}}/>
                 <Stack.Screen name={ERootScreens.RestApi} component={RestApiScreen} options={{title: 'Rest Api'}}/>
