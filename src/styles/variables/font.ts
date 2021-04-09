dd861d2f69ae808196bc6ec9fc5fd58911dbbf16
7ime <lopatsin1990@gmail.com>
2021-04-09 21:34:44 +0300

    styles variables: font

---
+++ src/styles/variables/font.ts
@@ -0,0 +1,20 @@
+export const FONT = {
+    family: {
+        os_bold: 'OpenSans-Bold',
+        os_semi_bold: 'OpenSans-SemiBold',
+        os_regular: 'OpenSans-Regular',
+        os_light: 'OpenSans-Light',
+    },
+    weight: {
+        bold: 700,
+        semi_bold: 600,
+        regular: 400,
+        light: 300
+    },
+    size: {
+        default: 16
+    }
+}
+
+export type IFont = typeof FONT
+
