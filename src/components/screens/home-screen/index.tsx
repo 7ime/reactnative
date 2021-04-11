dea85188549fa369c187c9376384e9c50b474ff2
7ime <lopatsin1990@gmail.com>
2021-04-11 15:12:32 +0300

    added package react-native-community/hooks

--- src/components/screens/home-screen/index.tsx
+++ src/components/screens/home-screen/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import styled from 'styled-components/native'
 import {
-    Text
+    Text, useWindowDimensions
 } from 'react-native'
 import {ERootScreens} from '../../../navigation/screens'
 import {INavigation} from '../../../navigation/model'
@@ -10,10 +11,46 @@ interface IProps extends INavigation.RootProps<ERootScreens.Home> {
 
 }
 
+const StyledRow = styled.View`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin: 20px 0;
+`
+
+const StyledCol = styled.View`
+  width: 50%;
+  padding: 0 10px;
+`
+
+const StyledDimsInfo = styled.View`
+  padding: 10px;
+  background-color: ${({ theme }) => theme.colors.white};
+`
+
 const HomeScreen = ({navigation}: IProps) => {
+    const windowDimensions = useWindowDimensions()
+
     return (
         <>
             <Text>Home Screen</Text>
+
+            <StyledRow>
+                <StyledCol>
+                    <StyledDimsInfo>
+                        <Text numberOfLines={1}>width: {windowDimensions.width}</Text>
+                        <Text numberOfLines={1}>height: {windowDimensions.height}</Text>
+                    </StyledDimsInfo>
+                </StyledCol>
+
+                <StyledCol>
+                    <StyledDimsInfo>
+                        <Text>font scale: {windowDimensions.fontScale}</Text>
+                        <Text>scale: {windowDimensions.scale}</Text>
+                    </StyledDimsInfo>
+                </StyledCol>
+            </StyledRow>
 
             <Button type={'primary'} onPress={() => navigation.navigate(ERootScreens.RestApi)}>Go to Rest Api Screen</Button>
         </>
